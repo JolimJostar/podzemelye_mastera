31df9e9fe6e63407dcea4fe2255b89ff0c02095a
Yan Kochkin <yavkochkin@ligastastavok.ru>
2026-03-13 15:25:51 +0300

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import * as path from 'path';
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
     build: {
         sourcemap: true,
     },
-    plugins: [react(), EnvironmentPlugin(['REACT_APP_TEXT'])],
+    plugins: [react()],
     publicDir: 'public',
     server: {
         host: true,
